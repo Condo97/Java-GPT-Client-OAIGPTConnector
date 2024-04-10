63ebf8643a2eddf674a4c840a884eab129ad51c8
Alex C <acoundou@gmail.com>
2024-04-09 22:49:28 -0400

    Working & tested :) Updated GPT models and other stuff.

--- lib/src/main/java/com/oaigptconnector/model/generation/OpenAIGPTModels.java
+++ lib/src/main/java/com/oaigptconnector/model/generation/OpenAIGPTModels.java
@@ -4,8 +4,8 @@ public enum OpenAIGPTModels {
 
     GPT_3_5_TURBO("gpt-3.5-turbo", false),
     GPT_3_5_TURBO_0613("gpt-3.5-turbo-0613", false),
-    GPT_4("gpt-4-0125-preview", false),
-    GPT_4_VISION("gpt-4-vision-preview", true),
+    GPT_4("gpt-4-turbo", false),
+    GPT_4_VISION("gpt-4-turbo", true),
     GPT_4_LONGINPUT("gpt-4-0125-preview", false),
     GPT_4_TURBO("gpt-4-0125-preview", false);
 
