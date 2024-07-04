638869221c884f3d5568059eae6da3b7ea62b5ae
Alex C <acoundou@gmail.com>
2024-07-03 21:43:14 -0400

    Added response format and stream options to base OAIChatCompletionRequest and made it so that things can be deserialized to Java objects better.

--- lib/src/main/java/com/oaigptconnector/model/request/chat/completion/OAIChatCompletionRequestStreamOptions.java
+++ lib/src/main/java/com/oaigptconnector/model/request/chat/completion/OAIChatCompletionRequestStreamOptions.java
@@ -1,5 +1,8 @@
 package com.oaigptconnector.model.request.chat.completion;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OAIChatCompletionRequestStreamOptions {
 
     private Boolean include_usage;
