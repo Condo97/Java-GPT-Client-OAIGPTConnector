812dd4c47170ca513d33d79c9852dad6cf5daac3
Alex C <acoundou@gmail.com>
2024-07-29 13:31:25 -0400

    Required choices in OAIGPTChatCompletionResponse to ensure its deserialization successfully throws if not successful and processes correctly if successful

--- lib/src/main/java/com/oaigptconnector/model/response/chat/completion/stream/OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction.java
+++ lib/src/main/java/com/oaigptconnector/model/response/chat/completion/stream/OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction.java
@@ -2,13 +2,15 @@ package com.oaigptconnector.model.response.chat.completion.stream;
 
 public class OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction {
 
+    private String name;
     private String arguments;
 
     public OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction() {
 
     }
 
-    public OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction(String arguments) {
+    public OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction(String name, String arguments) {
+        this.name = name;
         this.arguments = arguments;
     }
 
@@ -18,6 +20,14 @@ public class OpenAIGPTPromptChoiceDeltaStreamResponseToolCallFunction {
 
     public void setArguments(String arguments) {
         this.arguments = arguments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
